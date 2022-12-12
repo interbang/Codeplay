3aa1c3a28767375bae7afec027789c17aeebef66
Heather Hoffman <116114573+interbang@users.noreply.github.com>
2022-12-11 17:03:58 -0800

    Add files via upload

---
+++ guess_the_number.py
@@ -0,0 +1,17 @@
+y = 0
+
+x = int(input("Player 1: Pick a number between 1 and 10: "))
+z = int(input("Player 2: Guess the number: "))
+
+while True:
+    if z == 0:
+        print("Thanks for playing")
+        break
+    elif  z != x: 
+        z = int(input("That number is incorrect. Guess again: "))
+        y += 1
+        continue
+    elif z == 0:
+        y += 1
+        print(f"You got number {x} in {y} guess(es).")
+        break
